51ea99047e08025522f6b5fcf922de9d437b7346
WJH <hou32hou@gmail.com>
2017-07-30 15:36:51 +0800

    Copy function works properly now

--- WpfApplication1/WpfApplication1/XamlDisplayer.xaml.cs
+++ WpfApplication1/WpfApplication1/XamlDisplayer.xaml.cs
@@ -40,16 +40,7 @@ new FrameworkPropertyMetadata(default(object) , OnContentPropertyChanged));
         private static void OnContentPropertyChanged(DependencyObject d , DependencyPropertyChangedEventArgs e) {
             var xamlDisplayer = d as XamlDisplayer;
             var content = e.NewValue as Control;
-            if (content != null) {
-                content.Initialized += (sender , args) => {
-                    string xamlToBeDisplayed = Beautify(XamlWriter.Save(sender));
-                    if (xamlDisplayer != null) {
-                        xamlDisplayer.TextEditor.Text = xamlToBeDisplayed;
-                        xamlDisplayer.ContentPresenter.Content = sender;
-                    }
-                };
-            }
-
+            xamlDisplayer.ContentPresenter.Content = content;            
         }
        #endregion
 
@@ -66,22 +57,25 @@ new FrameworkPropertyMetadata(default(object) , OnContentPropertyChanged));
         public static readonly DependencyProperty XamlProperty =
             DependencyProperty.Register("Xaml" , typeof(string) , typeof(XamlDisplayer) , new PropertyMetadata("" , OnXamlPropertyChanged));
 
+        private string _codeToBeCopied;
+
         private static void OnXamlPropertyChanged(DependencyObject d , DependencyPropertyChangedEventArgs e) {
-            (d as XamlDisplayer).TextEditor.Text = (string)e.NewValue;
+            var xamlDisplayer = d as XamlDisplayer;
+            if (xamlDisplayer == null) return;
+            xamlDisplayer.TextEditor.Text = (string)e.NewValue;
+            xamlDisplayer._codeToBeCopied = (string)e.NewValue;
         }
 
         #endregion
-        private void CopyButton_OnClicked(object sender , RoutedEventArgs e) {
-            string toBeCopied = (string)(sender as Button).Tag;
-            Clipboard.SetDataObject(toBeCopied);
+        private void CopyButton_OnClicked(object sender , RoutedEventArgs e) {            
+            Clipboard.SetDataObject(_codeToBeCopied);
         }
 
         public static void HandleAllNode(Control host , XmlNode node) {
             if (node.LocalName == "XamlDisplayer") {
+                string xamlToBeDisplayed = Beautify(node.InnerXml);
                 (host.FindName(node.Attributes["x:Name"].Value) as XamlDisplayer)
-                    .Xaml =
-                    Beautify(node.InnerXml);
-
+                    .Xaml = xamlToBeDisplayed;                
             }
             else if (node.HasChildNodes) {
                 foreach (XmlNode child in node.ChildNodes) {
@@ -93,7 +87,7 @@ new FrameworkPropertyMetadata(default(object) , OnContentPropertyChanged));
         }
 
         private static string Beautify(string fullXaml) {
-            var styler = new StylerService(new StylerOptions());
+            var styler = new StylerService(new StylerOptions(){IndentWithTabs = true});
             string result = styler.StyleDocument(fullXaml);
             result = RemoveIrrelaventAttributes(result);
             result = RemoveEmptyLines(result);       
@@ -111,15 +105,19 @@ new FrameworkPropertyMetadata(default(object) , OnContentPropertyChanged));
                 char previousChar = '\0';
                 for (int i = 0; i < xaml.Length; i++) {
                     char currentChar = xaml[i];
-                    if (currentChar == '\r' && previousChar == '\n') {
-                        i += 2; //skip /r/n
-                        while(i+1 < xaml.Length && xaml[i+1]==' ')
-                        i ++; //skip the extra linefeed and spacing                        
+                    if (currentChar == '\r' && previousChar == '\n') {                        
+                        //skip \r,\n,\t
+                        while (i + 1 < xaml.Length &&
+                               (xaml[i + 1] == '\r' || 
+                               xaml[i + 1] == '\n' || 
+                               xaml[i + 1] == '\t')) {
+                            i++;
+                        }                                                
                     }
                     else {
                         sb.Append(currentChar);
                     }
-                    if (currentChar!=' ') previousChar = currentChar;
+                    if (currentChar!=' ' && currentChar!='\t') previousChar = currentChar;
                 }
                 return sb.ToString();
             }
