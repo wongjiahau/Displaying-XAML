6f3b6740d9730c00599fd0d008223e6c2ef3ae83
WJH <hou32hou@gmail.com>
2017-08-02 16:24:14 +0800

    2nd hotfix for dependency error

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -1,26 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Markup;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Xml;
-using System.Xml.Linq;
-using MaterialDesignThemes.Wpf;
-using DisplayXamlDemo;
-using Xavalon.XamlStyler.Core;
-using Xavalon.XamlStyler.Core.Options;
-using Path = System.IO.Path;
+
 //https://stackoverflow.com/questions/32690299/looping-through-all-nodes-in-xml-file-with-c-sharp\
 //https://github.com/Xavalon/XamlStyler
 namespace DisplayXamlDemo {
