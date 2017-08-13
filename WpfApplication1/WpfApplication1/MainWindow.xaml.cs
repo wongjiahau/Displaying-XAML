f3584bc4bc0998871e529a6aedeebda8e1a3f0c9
WJH <hou32hou@gmail.com>
2017-08-13 20:42:59 +0800

    Fixed the appearance of demo

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -1,10 +1,13 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Xml;
 using CodeDisplayer;
+using System.Windows.Data;
 
 //https://stackoverflow.com/questions/32690299/looping-through-all-nodes-in-xml-file-with-c-sharp\
 //https://github.com/Xavalon/XamlStyler
@@ -16,30 +19,36 @@ namespace DisplayXamlDemo {
         public MainWindow() {
             InitializeComponent();
             CodeDisplayer.XamlDisplayerPanel.Initialize(
-                source: 
-                    XamlDisplayerPanel.SourceEnum.LoadFromRemote ,
-                defaultLocalPath: 
+                source:
+                    XamlDisplayerPanel.SourceEnum.LoadFromLocal,
+                defaultLocalPath:
                     @"C:\Users\User\Source\Repos\Displaying-XAML\WpfApplication1\WpfApplication1\" ,
-                defaultRemotePath: 
+                defaultRemotePath:
                     "https://raw.githubusercontent.com/wongjiahau/Displaying-XAML/master/WpfApplication1/WpfApplication1/" ,
-                attributesToBeRemoved: 
+                attributesToBeRemoved:
                     new List<string>()
                     {
                         "xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"" ,
                         "xmlns:materialDesign=\"http://materialdesigninxaml.net/winfx/xaml/themes\"" ,
-                        "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\""  
+                        "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\""
                     }
                 );
+        }
+    }
 
-            MessageBox.Show(GetHost(XamlDisplayerPanel).GetType().Name);
-
+    public class BoolToDisplayModeConverter : IValueConverter{
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
+            var input = (bool) value;
+            if (input) {
+                return XamlDisplayer.DisplayModeEnum.TopBottom;
+            }
+            else {
+                return XamlDisplayer.DisplayModeEnum.LeftRight;
+            }
         }
 
-        public static DependencyObject GetHost(DependencyObject child) {
-            var parent = LogicalTreeHelper.GetParent(child);
-            if (parent == null) return child;
-            return GetHost(parent);
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
+            throw new NotImplementedException();
         }
-
     }
 }
