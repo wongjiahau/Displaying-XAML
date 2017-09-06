fa202b5ee53dde8f0e6ab20ef91c0d5bf4e1a1d5
WJH <hou32hou@gmail.com>
2017-09-06 16:51:57 +0800

    Refactored some code

--- XamlDisplayer/Demo/App.xaml.cs
+++ XamlDisplayer/Demo/App.xaml.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using System.Windows;
+using CodeDisplayer;
 
 namespace DisplayXamlDemo
 {
@@ -13,5 +15,23 @@ namespace DisplayXamlDemo
     /// </summary>
     public partial class App : Application
     {
+        private void App_OnStartup(object sender, StartupEventArgs e) {
+            XamlDisplayerPanel.Initialize(
+                source:
+                XamlDisplayerPanel.SourceEnum.LoadFromLocal ,
+                defaultLocalPath:
+                @"C:\Users\User\Source\Repos\Displaying-XAML\XamlDisplayer\Demo\" ,
+                defaultRemotePath:
+                "https://raw.githubusercontent.com/wongjiahau/Displaying-XAML/master/XamlDisplayer/Demo/" ,
+                attributesToBeRemoved:
+                new List<string>()
+                {
+                    "xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"" ,
+                    "xmlns:materialDesign=\"http://materialdesigninxaml.net/winfx/xaml/themes\"" ,
+                    "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\""
+                }
+            );
+
+        }
     }
 }
--- XamlDisplayer/Demo/MainWindow.xaml.cs
+++ XamlDisplayer/Demo/MainWindow.xaml.cs
@@ -19,21 +19,6 @@ namespace DisplayXamlDemo {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
-            CodeDisplayer.XamlDisplayerPanel.Initialize(
-                source:
-                    XamlDisplayerPanel.SourceEnum.LoadFromLocal,
-                defaultLocalPath:
-                    @"C:\Users\User\Source\Repos\Displaying-XAML\XamlDisplayer\Demo\" ,
-                defaultRemotePath:
-                    "https://raw.githubusercontent.com/wongjiahau/Displaying-XAML/master/XamlDisplayer/Demo/" ,
-                attributesToBeRemoved:
-                    new List<string>()
-                    {
-                        "xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"" ,
-                        "xmlns:materialDesign=\"http://materialdesigninxaml.net/winfx/xaml/themes\"" ,
-                        "xmlns:x=\"http://schemas.microsoft.com/winfx/2006/xaml\""
-                    }
-                );
             XamlDisplayerHost.Navigate(new Page1());
         }
     }
